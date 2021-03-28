5d3c93f9fcbe6f5603aa54a2ea3cbe1093ea6156
Lester Cardoz <lestercardoz11@gmail.com>
2021-03-28 21:05:21 +0530

    Testing workflow

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,6 +5,7 @@ function App() {
     <div className="App">
       <h1>Savi Portfolio</h1>
       <h2>Testing the workflow</h2>
+      <h3>Github Actions workflow done</h3>
     </div>
   );
 }
